4770b3d1314552bce0eafb9d7ebfacdb8673903f
zach_lysobey <zach@lysobey.com>
2015-08-20 18:38:10 -0400

    Add lint task with some eslint rules

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 'use strict';
 
 var gulp = require('gulp');
+var eslint = require('gulp-eslint');
 
-gulp.task('default', function() {
+gulp.task('default', ['lint'], function() {
     console.log('running default gulp task');
+});
+
+gulp.task('lint', function() {
+    return gulp.src('src/*.js')
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failOnError());
 });
--- src/hello.js
+++ src/hello.js
@@ -1,3 +1,3 @@
 function sayHello() {
-    return "Hello World!";
+    return 'Hello World!';
 }
